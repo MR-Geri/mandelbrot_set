47428b11ef4b07ff1b9473c068d2aacc8c847f6e
MR-Geri <ilya.kamckine@ya.ru>
2021-04-13 21:04:15 +0300

    update

--- main.py
+++ main.py
@@ -45,18 +45,13 @@ def mandelbrot_test(x, y, scale, maxiter):
         for j in range(1000):
             i0 = (i / scale + x) / 250 - 2
             j0 = (j / scale + y) / 250 - 2
-
             c = (i0 + j0 * 1j)
-
             z = 0
-
             for depth in range(maxiter):
                 z = f(z, c)
                 if abs(z) > 100:
                     break
-
             pic[i, j] = depth / maxiter
-
     return pic
 
 
@@ -64,21 +59,8 @@ def mandelbrot_draw(x, y, scale):
     global s
     pygame.init()
     win = pygame.display.set_mode((1000, 1000))
-    #
-    color = [(1 - (1 - q) ** 4, c) for q, c in zip(np.linspace(0, 1, 20), cycle(['#ffff88', '#000000', '#ffaa00']))]
-    cmap = clr.LinearSegmentedColormap.from_list('mycmap', color, N=2048)
-    plt.figure(figsize=(100, 100))
-    plt.xticks([])
-    plt.yticks([])
-    #
-    image = -mandelbrot_test(0, 0, 1, 1000).T
-    plt.imshow(image, cmap=cmap, interpolation='none')
-    plt.savefig('temp.png')
-    im = pygame.image.load('temp.png')
-    win.blit(im, (-130, -120, 1000, 1000))
-    #
-    # pic = mandelbrot(0, 0, 0.5, 1000)
-    # surfarray.blit_array(win, pic)
+    pic = mandelbrot(0, 0, 0.5, 1000)
+    surfarray.blit_array(win, pic)
     pygame.display.update()
     while True:
         for ev in pygame.event.get():
@@ -91,9 +73,23 @@ def mandelbrot_draw(x, y, scale):
                 scale *= 2
                 t = time.time()
                 pic = mandelbrot(x, y, scale, maxiter)
-                print(maxiter, time.time() - t)
+                print(f'{time.time() - t}\n{x, y, scale, maxiter}\n')
                 surfarray.blit_array(win, pic)
                 pygame.display.update()
+            if ev.type == pygame.KEYUP:
+                draw(x, y, scale, maxiter)
+
+
+def draw(x, y, scale, maxiter):
+    color = [(1 - (1 - q) ** 4, c) for q, c in zip(np.linspace(0, 1, 20), cycle(['#ffff88', '#000000', '#ffaa00']))]
+    cmap = clr.LinearSegmentedColormap.from_list('mycmap', color, N=2048)
+    plt.figure(figsize=(100, 100))
+    plt.xticks([])
+    plt.yticks([])
+    #
+    image = -mandelbrot_test(x, y, scale, maxiter).T
+    plt.imshow(image, cmap=cmap, interpolation='none')
+    plt.savefig('temp.png')
 
 
 if __name__ == '__main__':
